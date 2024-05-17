e6d53c97d37e0e01ba9bfc0c31899fce9d2f9ab9
Mike Wood <mikejwood@gmail.com>
2024-05-17 00:25:57 -0700

    all files added

--- ubuntu-dev.tf
+++ ubuntu-dev.tf
@@ -7,32 +7,7 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   key_name      = "mikew-1pass"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update -y
-              apt-get upgrade -y
-              apt-get install -y build-essential curl nginx npm
-
-              useradd -m -s /bin/bash mikew
-              echo 'mikew ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-              mkdir -p /home/mikew/.ssh
-              curl -o /home/mikew/.ssh/authorized_keys https://github.com/vicmike.keys
-              chown -R mikew:mikew /home/mikew/.ssh
-              chmod 700 /home/mikew/.ssh
-              chmod 600 /home/mikew/.ssh/authorized_keys
-
-              npm install -g nvm
-              npm install -g yarn
-              npm install -g typescript
-              npm install -g pm2
-
-              nvm install 12.15.0
-              nvm install 14.20.0
-              nvm use 14.20.0
-
-              mkdir -p /opt/docker
-              mkdir -p /opt/www
-              EOF
+user_data = file("${path.module}/cloud-init.yml")
 
   tags = {
     Name = "dev-template"
